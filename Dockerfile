cd43ef2f800e0a1afc0d29389c31856bbc0c4abc
Ramiro Berrelleza <ramiro@okteto.com>
2021-12-16 18:53:52 -0800

    basic pipeline running

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM golang:buster
 
 WORKDIR /app
 ADD . .
-RUN go build -o /usr/local/bin/hello-world
+RUN go build -o /usr/local/bin/hello-doppler
 
 EXPOSE 8080
-CMD ["/usr/local/bin/hello-world"]
+CMD ["/usr/local/bin/hello-doppler"]
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Starting hello-world server...")
+	fmt.Println("Starting hello-doppler server...")
 	http.HandleFunc("/", helloServer)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
